fff091d34f1b5f51d17217b0b506820f89112cc0
SkyBlueImpala <159174774+quacktastic@users.noreply.github.com>
2025-08-11 15:01:15 +0300

    Files and its classes have been structurized. Starting with CSV type of file(s).

---
+++ datamining-core/src/main/java/org/example/files/readerclasses/CSVReader.java
@@ -0,0 +1,4 @@
+package org.example.files.readerclasses;
+
+public class CSVReader {
+}
---
+++ datamining-core/src/main/java/org/example/files/readerclasses/Reader.java
@@ -0,0 +1,10 @@
+package org.example.files.readerclasses;
+
+
+/**
+ * Reader Interface provides flexibility and allows to make multiple reader classes that have
+ * same purpose with different file types. Users can add different types of files to make
+ * the software train itself.
+ */
+public interface Reader {
+}
